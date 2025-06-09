64dc27b850e70cdb6147e9339dfe4f85e33b4da5
Ranvijaya <ranvijayaofficial13.in@gmail.com>
2025-06-09 12:06:54 +0530

    This code is to reverse stack

---
+++ ReverseTheStack.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+class ReverseTheStack{
+    public static void main(String [] args){
+        Stack<Integer> stack=new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        reverseStack(stack);
+
+        while(!stack.isEmpty()){
+            System.out.print(stack.pop());
+        }
+    }
+    public static void reverseStack(Stack<Integer> stack){
+        if(stack.isEmpty()){
+            return;
+        }
+        int top=stack.pop();
+        reverseStack(stack);
+        pushAtBottom(stack, top);
+    }
+    public static void pushAtBottom(Stack<Integer> stack , int val) {
+        if (stack.isEmpty()) {
+            stack.push(val);
+            return;
+        }
+        int top = stack.pop();
+        pushAtBottom(stack, val);
+        stack.push(top);
+    }
+}
